01b468778d94929e2ce1739db6d78643e5b22124
aguirre537 <52295888+aguirre537@users.noreply.github.com>
2020-06-06 20:45:43 -0500

    Add files via upload

--- PikaCopter.ino
+++ PikaCopter.ino
@@ -5,76 +5,107 @@
 
 #define PWR_MGMT_1        0x6B
 #define PWR_MGMT_1_DATA   0x00
-// Bits from left to right:
-// Output Data Rate bit 1, Output Data Rate bit 0,
-// Bandwidth selection bit 1, Bandwidth selection bit 0,
-// Power enable, Z axis enable, Y axis enable, X axis enable
-
-#define GYRO_CTRL2_ADDR
-#define GYRO_CTRL2_DATA
-// Unused
-
-#define GYRO_CTRL3_ADDR
-#define GYRO_CTRL3_DATA
-// Unused
+//turn on
 
 #define GYRO_CONFIG       0x1B
 #define GYRO_CONFIG_DATA  0x08
-// Bits from left to right:
-// Block Data Update, 0=LSB 1=MSB,
-// Full Scale selection bit 1, Full Scale selection bit 0
-// Unused,
-// Self Test Enable bit 1, Self Test Enable bit 0,
-// Unused (0=4 1=3 Wire SPI)
+//DPS setting
 
-#define OUT_ALL     0x43
-#define OUT_X_H     0x43
-#define OUT_X_L     0x44
-#define OUT_Y_H     0x45
-#define OUT_Y_L     0x46
-#define OUT_Z_H     0x47
-#define OUT_Z_L     0x48
+#define ACC_CONFIG       0x1C
+#define ACC_CONFIG_DATA  0x10
+
+#define OUT_ALL         59
+#define OUT_X_H         0x43
+#define OUT_X_L         0x44
+#define OUT_Y_H         0x45
+#define OUT_Y_L         0x46
+#define OUT_Z_H         0x47
+#define OUT_Z_L         0x48
+
+#define TEMP_H          65
+#define TEMP_L          66
+
+#define ACC_OUT_X_H     59
+#define ACC_OUT_X_L     60
+#define ACC_OUT_Y_H     61
+#define ACC_OUT_Y_L     62
+#define ACC_OUT_Z_H     63
+#define ACC_OUT_Z_L     64
 
 typedef union{
-	i8 arr8[6];
-	i16 arr16[3];
-	struct{
-		union{
-			i16 full;
-			struct{
-				i8 high;
-				i8 low;
-			};
-		}x;
+  i8 arr8[14];
+  i16 arr16[7];
+  struct{
+    union{
+      i16 full;
+      struct{
+        i8 high;
+        i8 low;
+      };
+    }gyroX;
 
-		union{
-			i16 full;
-			struct{
-				i8 high;
-				i8 low;
-			};
-		}y;
+    union{
+      i16 full;
+      struct{
+        i8 high;
+        i8 low;
+      };
+    }gyroY;
 
-		union{
-			i16 full;
-			struct{
-				i8 high;
-				i8 low;
-			};
-		}z;
+    union{
+      i16 full;
+      struct{
+        i8 high;
+        i8 low;
+      };
+    }gyroZ;
+
+    union{
+      i16 full;
+      struct{
+        i8 high;
+        i8 low;
+      };
+    }temp;
+
+    union{
+      i16 full;
+      struct{
+        i8 high;
+        i8 low;
+      };
+    }accX;
+
+    union{
+      i16 full;
+      struct{
+        i8 high;
+        i8 low;
+      };
+    }accY;
+
+    union{
+      i16 full;
+      struct{
+        i8 high;
+        i8 low;
+      };
+    }accZ;
 
   };
-}GyroData;
+}MPU_Data;
 
   typedef union{
-	float arrf[2];
-	struct{
-	float pitch;
-	float roll;
-	};
+  float arrf[3];
+  struct{
+  float pitch;
+  float roll;
+  float yaw;
+  };
   }rotation_axis;
 
-GyroData calibrationData;
+MPU_Data calibrationData;
+long acc_total_vector;
 
 static inline
 void gyroWrite8(const u8 reg, const u8 data)
@@ -85,14 +116,24 @@ void gyroWrite8(const u8 reg, const u8 data)
   Wire.endTransmission();
 }
 
-void gyroReadXYZ(GyroData *data)
+void gyroReadXYZ(MPU_Data *data)
 {
   Wire.beginTransmission(GYRO_ADDR);
   Wire.write(OUT_ALL);
   Wire.endTransmission();
-  Wire.requestFrom(GYRO_ADDR, 6);
-  while(Wire.available()<6);
+  Wire.requestFrom(GYRO_ADDR, 14);
+  while(Wire.available()<14);
 
+  data->arr8[9] = Wire.read();
+  data->arr8[8] = Wire.read();
+  data->arr8[11] = Wire.read();
+  data->arr8[10] = Wire.read();
+  data->arr8[13] = Wire.read();
+  data->arr8[12] = Wire.read();
+  
+  data->arr8[7] = Wire.read();
+  data->arr8[6] = Wire.read();
+  
   data->arr8[1] = Wire.read();
   data->arr8[0] = Wire.read();
   data->arr8[3] = Wire.read();
@@ -105,11 +146,11 @@ void gyroReadXYZ(GyroData *data)
   }
 }
 
-void gyroAverageReads(GyroData *reads)
+void gyroAverageReads(MPU_Data *reads)
 {
   for(u8 read1 = 0; read1 < 32; read1++){
     for(u8 axis = 0; axis < 3; axis++){
-	calibrationData.arr16[axis] += reads[read1].arr16[axis]/32;
+  calibrationData.arr16[axis] += reads[read1].arr16[axis]/32;
     }
   }
 }
@@ -120,8 +161,9 @@ void gyroInit(void)
   Wire.begin();
   gyroWrite8(PWR_MGMT_1, PWR_MGMT_1_DATA);
   gyroWrite8(GYRO_CONFIG, GYRO_CONFIG_DATA);
-  GyroData calibrationReads[32] = {0};
-  //GyroData calibrationAverages = {0};
+  gyroWrite8(ACC_CONFIG, ACC_CONFIG_DATA);
+  MPU_Data calibrationReads[32] = {0};
+  //MPU_Data calibrationAverages = {0};
   for(u8 read1 = 0; read1 < 32; read1++){
     gyroReadXYZ(&calibrationReads[read1]);
   }
@@ -146,22 +188,54 @@ void gyroPrintData(rotation_axis* data)
 
 void setup()
 {
-	Serial.begin(9600);
-	GyroData angles;
-	gyroInit();
+  Serial.begin(9600);
+  MPU_Data angles;
+  gyroInit();
 
-	while(1){
-		static rotation_axis rotation_values = {0};
-		u32 time1 = 0;
-		u32 time2 = micros();
-		if(time2 - time1 >= 4000){
-			gyroReadXYZ(&angles);
-			rotation_values.pitch += (float)angles.x.full*0.0000611f;
-			rotation_values.roll += (float)angles.y.full*0.0000611f;
-			time1 = time2;
-			gyroPrintData(&rotation_values);
-		}
-	}
+  while(1){
+    static rotation_axis gyro_values = {0};
+    static rotation_axis acc_values = {0};
+    static rotation_axis final_values = {0};
+    static u32 time1 = 0;
+    static u32 time2 = 0;
+
+    
+    if(micros() - time1 >= 4000){
+      gyroReadXYZ(&angles);
+      gyro_values.pitch += angles.gyroX.full*0.0000611f;
+      gyro_values.roll += angles.gyroY.full*0.0000611f;
+      //gyro_values.roll -= gyro_values.pitch*sin((float)angles.gyroZ.full*0.000001066f);//acounts for yaw change
+      //gyro_values.pitch += gyro_values.roll*sin((float)angles.gyroZ.full*0.000001066f);
+
+      //I stole the lines//Accelerometer angle calculations
+      acc_total_vector = sqrt(((long)angles.accX.full*(long)angles.accX.full)+((long)angles.accY.full*(long)angles.accY.full)+((long)angles.accZ.full*(long)angles.accZ.full));       //Calculate the total accelerometer vector.
+      
+      if(abs(angles.accX.full) < acc_total_vector){                                        //Prevent the asin function to produce a NaN
+        acc_values.pitch = asin((float)angles.accY.full/acc_total_vector)* 57.296;          //Calculate the pitch angle.
+      }
+      if(abs(angles.accX.full) < acc_total_vector){                                        //Prevent the asin function to produce a NaN
+        acc_values.roll = asin((float)angles.accX.full/acc_total_vector)* -57.296;          //Calculate the roll angle.
+      }
+
+      //Place the MPU-6050 spirit level and note the values in the following two lines for calibration.
+      acc_values.pitch -= -3.30;                                                   //Accelerometer calibration value for pitch.
+      acc_values.roll -= 6.30;                                                    //Accelerometer calibration value for roll.
+      
+      final_values.pitch = gyro_values.pitch * 0.9996 + acc_values.pitch * 0.0004;            //Correct the drift of the gyro pitch angle with the accelerometer pitch angle.
+      final_values.roll = gyro_values.roll * 0.9996 + acc_values.roll * 0.0004;               //Correct the drift of the gyro roll angle with the accelerometer roll angle.
+    
+      time1 = micros();
+    }
+    if(micros() -  time2 >= 1000000){
+//      Serial.println(angles.accX.full);
+//      Serial.println(angles.accY.full);
+//      Serial.println(angles.accZ.full);
+//      Serial.println(acc_total_vector);
+//      Serial.println("");
+      gyroPrintData(&acc_values);
+      time2 = micros();
+    }
+  }
 }
 
 void loop()
